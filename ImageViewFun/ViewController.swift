9dab9cf49fec3e3b07e57338b8f065961ccd9da7
Janusz Chudzynski <appzzman>
2015-02-12 10:00:10 -0600

    Initial Commit

---
+++ ImageViewFun/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  ImageViewFun
+//
+//  Created by Janusz Chudzynski on 2/12/15.
+//  Copyright (c) 2015 Janusz Chudzynski. All rights reserved.
+//  All images were downloaded from internet. I am not an owner of them.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var frames = [UIImage]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.getURLOfImage()
+        
+        for index in 0...23 {
+            var filename = "tmp-\(index).gif"
+            var image = UIImage(named: filename)
+            frames += [image!]
+        }
+        self.imageView.image = UIImage(named: "dog.jpg")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func animate(sender: AnyObject) {
+        self.imageView.animationDuration = 1
+        self.imageView.animationImages = frames
+        self.imageView.animationRepeatCount = 1
+        self.imageView.startAnimating()
+    }
+    
+    func getURLOfImage(){
+        let fileURL = NSBundle.mainBundle().URLForResource("cat", withExtension: "png")
+        println(fileURL)
+        
+    }
+}
+
